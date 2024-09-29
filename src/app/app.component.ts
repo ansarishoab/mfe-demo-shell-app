ac0b15262341081725fbebb472707f8d410866aa
ansarishoab <shoab.8055@gmail.com>
2024-09-29 23:36:59 +0530

    project setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,35 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { RouterOutlet, Router, Routes } from '@angular/router';
+import { loadRemoteModule } from '@angular-architects/module-federation';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.less'
 })
 export class AppComponent {
   title = 'shell-app';
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    const dynamicRoutes: any = [
+      {
+        path: 'mfe2',
+        loadChildren: () =>
+          loadRemoteModule({
+            type: 'module',
+            remoteEntry: 'http://localhost:4202/remoteEntry.js',
+            exposedModule: './Routes',
+          }).then((m) => m.remoteRoutes),
+      },
+    ]
+    console.log('AppComponent initialized', this.router.config);
+    this.router.resetConfig([...this.router.config, ...dynamicRoutes]);
+  }
 }
