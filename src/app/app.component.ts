19b9aafcfe10f300915532087b5cd787991959a0
ansarishoab <shoab.8055@gmail.com>
2024-09-30 22:49:26 +0530

    added env and some pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './core/header/header.component';
 import { FooterComponent } from './core/footer/footer.component';
+import { PageNotFoundComponent } from './core/page-not-found/page-not-found.component';
 import { RouterOutlet, Router, Routes } from '@angular/router';
 import { loadRemoteModule } from '@angular-architects/module-federation';
-
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.less'
+  styleUrl: './app.component.less',
 })
 export class AppComponent {
   title = 'shell-app';
@@ -18,18 +19,26 @@ export class AppComponent {
   constructor(private router: Router) {}
 
   ngOnInit() {
-    const dynamicRoutes: any = [
-      {
-        path: 'mfe2',
-        loadChildren: () =>
-          loadRemoteModule({
-            type: 'module',
-            remoteEntry: 'http://localhost:4202/remoteEntry.js',
-            exposedModule: './Routes',
-          }).then((m) => m.remoteRoutes),
-      },
-    ]
-    console.log('AppComponent initialized', this.router.config);
-    this.router.resetConfig([...this.router.config, ...dynamicRoutes]);
+    if (environment.production && false) {
+      //call the api to get actual routes
+    } else {
+      const dynamicRoutes: any = [
+        {
+          path: 'mfe2',
+          loadChildren: () =>
+            loadRemoteModule({
+              type: 'module',
+              remoteEntry: 'http://localhost:4202/remoteEntry.js',
+              exposedModule: './Routes',
+            }).then((m) => m.remoteRoutes),
+        },
+      ];
+      this.router.resetConfig([...this.router.config, ...dynamicRoutes]);
+    }
+    const wildCardRoute: any = {
+      path: '**',
+      component: PageNotFoundComponent,
+    };
+    this.router.resetConfig([...this.router.config, wildCardRoute]);
   }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { HomeComponent } from './core/home/home.component';
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+    }
+];
